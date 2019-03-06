b9c6e9ac9a111fcd9b1bbe801c3a8fa3fba69854
xieyalong <535030765@qq.com>
2019-03-06 10:54:56 +0800

    -----

--- sample/src/main/java/org/litepal/litepalsample/MyApplication.java
+++ sample/src/main/java/org/litepal/litepalsample/MyApplication.java
@@ -1,6 +1,8 @@
 package org.litepal.litepalsample;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
 
 import org.litepal.LitePal;
 
@@ -10,5 +12,41 @@ public class MyApplication extends Application{
     public void onCreate() {
         super.onCreate();
         LitePal.initialize(this);
+        registerActivity();
+    }
+
+    public  void registerActivity(){
+        registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle bundle) {
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+                System.out.println(">]Activity="+activity.getClass().getName());
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {//监控进入和离开的状态
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+            }
+        });
     }
 }
